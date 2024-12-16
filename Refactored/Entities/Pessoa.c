0adb423ebda331d8af4521a5ae10d4f54112a7b2
Samuel-Pinheiro-C-Lopes <samuelpinheiroclopes@gmail.com>
2024-12-16 15:16:07 -0300

    Estrutura de pessoa feita com encapsulamento

---
+++ Refactored/Entities/Pessoa.c
@@ -0,0 +1,14 @@
+#include "Pessoa.h"
+
+#pragma region Implementação de Pessoa
+
+struct Pessoa 
+{
+    int Matricula;
+    int Idade;
+    char Sexo;
+    int Data_Nascimento[8];
+    int CPF[11];
+};
+
+#pragma endregion
